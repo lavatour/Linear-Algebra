e1187a4d4abceca7f5ad40439a1464d0d6c399b6
lavatour <ray.dykeman@gmail.com>
2022-01-25 19:07:37 +0000

    Gauss Jordan elimination without row exchange.

--- main.py
+++ main.py
@@ -11,4 +11,20 @@ matrix = MatrixMath()
 lectures = Lectures()
 
 
-#lectures.lecture61()
+A = matrix.randomMatrix(3, 3, -2, 2, int)
+print(A)
+# Gauss elimination
+for i in range(len(A)):
+    A[i] = A[i] / A[i][i]
+    for l in range(i + 1, len(A)):
+        A[l] = A[l] - A[l][i] * A[i]
+        #print(i, l)
+#print(A)
+#print()
+for m in range(len(A)-1, -1, -1):
+    #print(i)
+    for l in range(len(A)-1, m, -1):
+        #print(m, l)
+        A[m] = A[m] - A[m][l]*A[l]
+print()
+print(A)
